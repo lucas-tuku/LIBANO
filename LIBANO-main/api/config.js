580036a7b5aa886afecdc2bf46f93f7c685f9968
lucas-tuku <lucastuku69@gmail.com>
2026-06-23 10:34:47 -0300

    Add files via upload

---
+++ LIBANO-main/api/config.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  
+  res.status(200).json({
+    SUPABASE_URL: process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+    SUPABASE_KEY: process.env.SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
+  });
+}
